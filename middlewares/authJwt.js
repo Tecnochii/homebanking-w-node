50260b6173c26dd3723ba816b45d1c95bd8fadf9
Emmanuel Leiva <ema62x@gmail.com>
2022-05-04 18:08:32 -0300

    arreglado el error de jwt

--- middlewares/authJwt.js
+++ middlewares/authJwt.js
@@ -5,33 +5,36 @@ const Client = require('../models/Client')
 
 const verifyToken = async (req, res, next) => {
     const token = req.cookies["x-acces-token"]
-
+    console.log(token)
     try {
         if (!token) {
             res.redirect('/')
-        }
-
-        const decoded = jwt.verify(token, config.secret)
-
-        if (!decoded) {
-            return res.json(404).json('invalid token')
-        }
-
-        const client = await Client.findOne({
-            where: {
-                id: decoded.id
-            }
-        })
-
-        if (!client) {
-            return json.status(400).json({
-                message: 'client not exist'
-            })
         } else {
-            next()
+            const decoded = jwt.verify(token, config.secret)
+
+            if (!decoded) {
+                return res.json(404).json('invalid token')
+            }
+
+            const client = await Client.findOne({
+                where: {
+                    id: decoded.id
+                }
+            })
+
+            if (!client) {
+                return json.status(400).json({
+                    message: 'client not exist'
+                })
+            } else {
+                next()
+            }
+            console.log(decoded)
         }
-        console.log(decoded)
+
+
     } catch (error) {
+        console.log(1, error);
         res.clearCookie("x-acces-token")
         res.status(401).json({ message: 'Unauthorized or Session Expired' })
     }
